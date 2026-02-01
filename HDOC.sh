e55ab82c7603cd2e8b7298b86f0edc15bdf6a145
Vyom73 <vyom3369@gmail.com>
2026-02-01 12:00:00 +0530

    commit on 2026-02-01

--- HDOC.sh
+++ HDOC.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-start_date="2024-01-01"
-end_date="2024-12-31"
+start_date="2026-02-01"
+end_date="2026-04-27"
 current_date=$start_date
 
 while [[ "$current_date" < "$end_date" || "$current_date" == "$end_date" ]]
 do
   skip=$((RANDOM % 7))
-  if [[ $skip -ne 1 && $skip -ne 5 ]]; then
+if [[ $skip -ne 1 && $skip -ne 3 && $skip -ne 5 ]]; then
     commits=$((RANDOM % 4 + 2))
     for i in $(seq 1 $commits)
     do
